2561109f4bcc85b4892cefab14c6b959d370dfae
MLamagna <marcus@mlamagna.com.br>
2023-10-30 14:37:06 -0300

    Finalizado projeto consumo de API

--- lib/Home.dart
+++ lib/Home.dart
@@ -8,21 +8,34 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
-  //Metodo responsável para recuperar o cep via API
+  //Criando um controlador de Cep
+  TextEditingController _controllerCep = TextEditingController();
+
+  //Capturar o resultado direto no app
+  String _resultado = "Resultado";
+
+  //Metodo responsável para recuperar os dados via API
   _recuperarCep() async {
-    String url = "https://viacep.com.br/ws/05566030/json";
+    String cepDigitado = _controllerCep.text;
+    String url = "https://viacep.com.br/ws/${cepDigitado}/json/";
     http.Response response;
 
     response = await http.get(Uri.parse(url));
+    print("resposta: " + response.body);
     //Decodificar um objeto em json
-    Map<String, dynamic> retorno = json.decode(response.body);
+    Map<dynamic, dynamic> retorno = json.decode(response.body);
+    String cep = retorno["cep"];
     String logradouro = retorno["logradouro"];
-    String complemento = retorno["complemento"];
+    //String complemento = retorno["complemento"];
     String bairro = retorno["bairro"];
     String localidade = retorno["localidade"];
 
+    setState(() {
+      _resultado = "${cep}, ${logradouro}, ${bairro}, ${localidade}";
+    });
+
     print(
-        "Resposta logradouro: ${logradouro} complemento: ${complemento} bairro: ${bairro} localidade: ${localidade}");
+        "Resposta cep: ${cep} logradouro: ${logradouro} bairro: ${bairro} localidade: ${localidade}");
 
     //print("resposta: " + response.body);
     //print("resposta: " + response.statusCode.toString());
@@ -38,7 +51,16 @@ class _HomeState extends State<Home> {
         padding: EdgeInsets.all(40),
         child: Column(
           children: [
-            ElevatedButton(onPressed: _recuperarCep, child: Text("Clique aqui"))
+            TextField(
+              keyboardType: TextInputType.number,
+              decoration:
+                  InputDecoration(labelText: "Digite o cep: exemplo: 05566030"),
+              style: TextStyle(fontSize: 20),
+              controller: _controllerCep,
+            ),
+            ElevatedButton(
+                onPressed: _recuperarCep, child: Text("Clique aqui")),
+            Text(_resultado)
           ],
         ),
       ),
